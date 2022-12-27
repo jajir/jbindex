766c65842c41070d13e05360a6fb54f13d58f494
jajir <jirout@gmail.com>
2022-12-27 11:52:12 +0100

    code was reformated

--- src/main/java/com/coroptis/store/StoreStreamer.java
+++ src/main/java/com/coroptis/store/StoreStreamer.java
@@ -8,13 +8,13 @@ import com.coroptis.index.type.TypeReader;
 
 public class StoreStreamer<K, V> extends FileStreamer<K, V> implements CloseableResource {
 
-	public StoreStreamer(final Directory directory, final TypeReader<K> keyReader, final TypeReader<V> valueReader) {
-		super(directory, StoreWriter.STORE, keyReader, valueReader);
-	}
+    public StoreStreamer(final Directory directory, final TypeReader<K> keyReader, final TypeReader<V> valueReader) {
+	super(directory, StoreWriter.STORE, keyReader, valueReader);
+    }
 
-	public StoreStreamer(final Directory directory, final Class<?> keyClass, final Class<?> valueClass) {
-		super(directory, StoreWriter.STORE, TypeConvertors.getInstance().get(keyClass, OperationType.READER),
-				TypeConvertors.getInstance().get(valueClass, OperationType.READER));
-	}
+    public StoreStreamer(final Directory directory, final Class<?> keyClass, final Class<?> valueClass) {
+	super(directory, StoreWriter.STORE, TypeConvertors.getInstance().get(keyClass, OperationType.READER),
+		TypeConvertors.getInstance().get(valueClass, OperationType.READER));
+    }
 
 }
