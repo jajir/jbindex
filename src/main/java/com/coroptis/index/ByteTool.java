98f4b81d88b8f9c62c47a1f80f15d35931a56610
jajir <jirout@gmail.com>
2023-06-02 10:05:28 +0200

    java doc and formatting was added

--- src/main/java/com/coroptis/index/ByteTool.java
+++ src/main/java/com/coroptis/index/ByteTool.java
@@ -1,19 +1,27 @@
 package com.coroptis.index;
+
 /**
- * Simple operations with byte arrays. 
+ * Simple operations with byte arrays.
  * 
  * @author jan
  * 
  */
 public class ByteTool {
-    
+
+    /**
+     * Count how many byte is same in two byte arrays.
+     * 
+     * @param array1 required first array
+     * @param array2 required second array
+     * @return number of same bytes
+     */
     public int howMuchBytesIsSame(final byte[] array1, final byte[] array2) {
-        if(array1==null){
+        if (array1 == null) {
             throw new NullPointerException("First byte array is null.");
         }
-        if(array2==null){
+        if (array2 == null) {
             throw new NullPointerException("Second byte array is null.");
-        }        
+        }
         int sameBytes = 0;
         while (true) {
             if (sameBytes >= array1.length) {
@@ -30,7 +38,15 @@ public class ByteTool {
         }
     }
 
-    public byte[] getRemainingBytesAfterIndex(final int index, final byte[] full) {
+    /**
+     * Get part of byte array after given index.
+     * 
+     * @param index required index in byte array
+     * @param full  required byte array
+     * @return byte array
+     */
+    public byte[] getRemainingBytesAfterIndex(final int index,
+            final byte[] full) {
         final byte[] out = new byte[full.length - index];
         System.arraycopy(full, index, out, 0, out.length);
         return out;
