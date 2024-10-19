2d9368ac836bb6adbbdaea7076be309349338951
Jan Jirout <jirout@gmail.com>
2024-10-19 15:56:37 +0000

    added complex test for index #3

--- src/test/java/com/coroptis/index/sst/IndexTest.java
+++ src/test/java/com/coroptis/index/sst/IndexTest.java
@@ -1,126 +1,100 @@
 package com.coroptis.index.sst;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.stream.Collectors;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.api.BeforeEach;
 
-import com.coroptis.index.Pair;
 import com.coroptis.index.datatype.TypeDescriptorInteger;
 import com.coroptis.index.datatype.TypeDescriptorString;
 import com.coroptis.index.directory.Directory;
 import com.coroptis.index.directory.MemDirectory;
-import com.coroptis.index.segment.SegmentId;
 
 /**
- * Basic index integrations tests.
+ * Test insert random key value pairs than randomly change them and finally
+ * verify the correctness of data.
  */
-public class IndexTest extends AbstractIndexTest {
+public class IndexTest {
+
     final Directory directory = new MemDirectory();
-    final SegmentId id = SegmentId.of(27);
     final TypeDescriptorString tds = new TypeDescriptorString();
     final TypeDescriptorInteger tdi = new TypeDescriptorInteger();
 
-    private final List<Pair<Integer, String>> testData = List.of(
-            Pair.of(1, "bbb"), Pair.of(2, "ccc"), Pair.of(3, "dde"),
-            Pair.of(4, "ddf"), Pair.of(5, "ddg"), Pair.of(6, "ddh"),
-            Pair.of(7, "ddi"), Pair.of(8, "ddj"), Pair.of(9, "ddk"),
-            Pair.of(10, "ddl"), Pair.of(11, "ddm"));
+    private Index<String, Integer> index;
+    private Map<String, Integer> referenceMap;
+    private Random random;
 
-    @Test
-    void testBasic() throws Exception {
-        final Index<Integer, String> index = makeSstIndex(false);
-        writePairs(index, testData);
-
-        /**
-         * Calling of verifyIndexData before compact() will fail. It's by design.
-         */
-
-        verifyIndexSearch(index, testData);
-        index.compact();
-
-        verifyIndexData(index, testData);
-        verifyIndexSearch(index, testData);
-
-        index.close();
+    @BeforeEach
+    public void setUp() {
+        index = makeSstIndex(false);
+        referenceMap = new HashMap<>();
+        random = new Random();
     }
 
-    @Test
-    void test_duplicated_operations() throws Exception {
-        final Index<Integer, String> index = makeSstIndex(false);
-        for (int i = 0; i < 100; i++) {
-            index.put(i, "kachna");
-            index.delete(i);
+    private final static int TEST_ITEMS = 10000;
+    private final static int TEST_UPDATE_LOOPS = 2;
+    private final static int TEST_STRING_LENGTH = 10;
+
+    public void testRandomOperations() {
+        // Step 1: Insert random key-value pairs
+        for (int i = 0; i < TEST_ITEMS; i++) {
+            final String key = generateRandomString();
+            final int value = random.nextInt();
+            index.put(key, value);
+            referenceMap.put(key, value);
         }
-        index.compact();
-        verifyIndexData(index, new ArrayList<>());
+
+        // Step 2: Randomly change some entries
+        for (int i = 0; i < TEST_UPDATE_LOOPS; i++) {
+            final List<String> keys = referenceMap.keySet().stream().collect(Collectors.toList());
+            for (final String key : keys) {
+                if (random.nextInt(10) % 3 == 0) {
+                    // key is deleted
+                    index.delete(key);
+                    referenceMap.remove(key);
+                } else if (random.nextInt(10) % 3 == 1) {
+                    // new key is inserted
+                    final String newKey = generateRandomString();
+                    final int newValue = random.nextInt();
+                    index.put(newKey, newValue);
+                    referenceMap.put(newKey, newValue);
+                } else {
+                    // key is updated
+                    final int newValue = random.nextInt();
+                    index.put(key, newValue);
+                    referenceMap.put(key, newValue);
+                }
+            }
+        }
+
+        // Step 3: Verify the correctness of the remaining entries
+        for (Map.Entry<String, Integer> entry : referenceMap.entrySet()) {
+            assertEquals(entry.getValue(), index.get(entry.getKey()));
+        }
+
     }
 
-    @Test
-    void test_delete_search_operations() throws Exception {
-        final Index<Integer, String> index = makeSstIndex(false);
-        for (int i = 0; i < 300; i++) {
-            index.put(i, "kachna");
-            assertEquals("kachna", index.get(i));
-            index.delete(i);
-            assertNull(index.get(i));
-            verifyIndexData(index, List.of());
+    private String generateRandomString() {
+        final StringBuilder sb = new StringBuilder(TEST_STRING_LENGTH);
+        for (int i = 0; i < TEST_STRING_LENGTH; i++) {
+            final char c = (char) (random.nextInt(26) + 'a');
+            sb.append(c);
         }
-        verifyIndexData(index, List.of());
+        return sb.toString();
     }
 
-    /**
-     * In this test getStream() could ommit some results
-     * @param iterations
-     * @throws Exception
-     */
-    @ParameterizedTest
-    @CsvSource(value = {"1:0", "3:3", "5:3", "15:15", "100:99", "102:102"}, delimiter = ':')
-    void test_adds_and_deletes_operations_no_compacting(final int iterations, final int itemsInIndex) throws Exception {
-        final Index<Integer, String> index = makeSstIndex(false);
-        for (int i = 0; i < iterations; i++) {
-            index.put(i, "kachna");
-            assertEquals("kachna", index.get(i));
-        }
-        assertEquals(itemsInIndex, index.getStream().count());
-        for (int i = 0; i < iterations; i++) {
-            index.delete(i);
-            assertNull(index.get(i));
-        }
-        verifyIndexData(index, List.of());
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = {1, 3, 5, 15, 100, 102})
-    void test_adds_and_deletes_operations_with_compacting(final int iterations) throws Exception {
-        final Index<Integer, String> index = makeSstIndex(false);
-        for (int i = 0; i < iterations; i++) {
-            index.put(i, "kachna");
-            assertEquals("kachna", index.get(i));
-        }
-        index.compact();        
-        assertEquals(iterations, index.getStream().count());
-        for (int i = 0; i < iterations; i++) {
-            index.delete(i);
-            assertNull(index.get(i));
-        }
-        index.compact();
-        verifyIndexData(index, List.of());
-    }
-
-    private Index<Integer, String> makeSstIndex(boolean withLog) {
-        return Index.<Integer, String>builder()//
+    private Index<String, Integer> makeSstIndex(boolean withLog) {
+        return Index.<String, Integer>builder()//
                 .withDirectory(directory)//
-                .withKeyClass(Integer.class)//
-                .withValueClass(String.class)//
-                .withKeyTypeDescriptor(tdi) //
-                .withValueTypeDescriptor(tds) //
+                .withKeyClass(String.class)//
+                .withValueClass(Integer.class)//
+                .withKeyTypeDescriptor(tds) //
+                .withValueTypeDescriptor(tdi) //
                 .withCustomConf()//
                 .withMaxNumberOfKeysInSegment(2) //
                 .withMaxNumberOfKeysInSegmentCache(1) //
@@ -131,5 +105,4 @@ public class IndexTest extends AbstractIndexTest {
                 .withUseFullLog(withLog) //
                 .build();
     }
-
 }
