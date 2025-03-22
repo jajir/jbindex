1ef3a62bf69acb67aa9f95246bd673d9dd8f9d45
Jan Jirout <jirout@gmail.com>
2025-03-22 08:31:34 +0100

    test was improved #59

--- core/src/main/java/com/coroptis/index/sorteddatafile/SortedDataFileWriter.java
+++ core/src/main/java/com/coroptis/index/sorteddatafile/SortedDataFileWriter.java
@@ -12,131 +12,122 @@ import com.coroptis.index.directory.FileWriter;
 
 public class SortedDataFileWriter<K, V> implements CloseableResource {
 
-        private final TypeWriter<V> valueWriter;
+    private final TypeWriter<V> valueWriter;
 
-        private final FileWriter fileWriter;
+    private final FileWriter fileWriter;
 
-        private final TypeDescriptor<K> keyTypeDescriptor;
+    private final Comparator<K> keyComparator;
 
-        private final Comparator<K> keyComparator;
+    private final ConvertorToBytes<K> keyConvertorToBytes;
 
-        private final ConvertorToBytes<K> keyConvertorToBytes;
+    private long position;
 
-        private long position;
+    private DiffKeyWriter<K> diffKeyWriter;
 
-        private DiffKeyWriter<K> diffKeyWriter;
+    private K previousKey = null;
 
-        private K previousKey = null;
+    public SortedDataFileWriter(final TypeWriter<V> valueWriter,
+            final FileWriter fileWriter,
+            final TypeDescriptor<K> keyTypeDescriptor) {
+        this.valueWriter = Objects.requireNonNull(valueWriter,
+                "valueWriter is required");
+        this.fileWriter = Objects.requireNonNull(fileWriter,
+                "fileWriter is required");
+        Objects.requireNonNull(keyTypeDescriptor,
+                "keyTypeDescriptor is required");
+        this.keyComparator = Objects.requireNonNull(
+                keyTypeDescriptor.getComparator(), "Comparator is required");
+        this.keyConvertorToBytes = Objects.requireNonNull(
+                keyTypeDescriptor.getConvertorToBytes(),
+                "Convertor to bytes is required");
+        this.diffKeyWriter = makeDiffKeyWriter();
+        position = 0;
+    }
 
-        public SortedDataFileWriter(final TypeWriter<V> valueWriter,
-                        final FileWriter fileWriter,
-                        final TypeDescriptor<K> keyTypeDescriptor) {
-                this.valueWriter = Objects.requireNonNull(valueWriter,
-                                "valueWriter is required");
-                this.fileWriter = Objects.requireNonNull(fileWriter,
-                                "fileWriter is required");
-                this.keyTypeDescriptor = Objects.requireNonNull(
-                                keyTypeDescriptor,
-                                "keyTypeDescriptor is required");
-                this.keyComparator = Objects.requireNonNull(
-                                keyTypeDescriptor.getComparator(),
-                                "Comparator is required");
-                this.keyConvertorToBytes = Objects.requireNonNull(
-                                keyTypeDescriptor.getConvertorToBytes(),
-                                "Convertor to bytes is required");
-                this.diffKeyWriter = makeDiffKeyWriter();
-                position = 0;
+    private DiffKeyWriter<K> makeDiffKeyWriter() {
+        return new DiffKeyWriter<>(keyConvertorToBytes, keyComparator);
+    }
+
+    /**
+     * Verify that keys are in correct order.
+     * 
+     * @param key
+     */
+    private void verifyKeyOrder(final K key) {
+        if (previousKey != null) {
+            final int cmp = keyComparator.compare(previousKey, key);
+            if (cmp == 0) {
+                final String s2 = new String(keyConvertorToBytes.toBytes(key));
+                final String keyComapratorClassName = keyComparator.getClass()
+                        .getSimpleName();
+                throw new IllegalArgumentException(String.format(
+                        "Attempt to insers same key as previous. Key '%s' was comapred with '%s'",
+                        s2, keyComapratorClassName));
+            }
+            if (cmp > 0) {
+                final String s1 = new String(
+                        keyConvertorToBytes.toBytes(previousKey));
+                final String s2 = new String(keyConvertorToBytes.toBytes(key));
+                final String keyComapratorClassName = keyComparator.getClass()
+                        .getSimpleName();
+                throw new IllegalArgumentException(String.format(
+                        "Attempt to insers key in invalid order. "
+                                + "Previous key is '%s', inserted key is '%s' and comparator is '%s'",
+                        s1, s2, keyComapratorClassName));
+            }
         }
+        previousKey = key;
+    }
 
-        private DiffKeyWriter<K> makeDiffKeyWriter() {
-                return new DiffKeyWriter<>(keyConvertorToBytes, keyComparator);
-        }
+    /**
+     * Allows to put new key value pair into index.
+     *
+     * @param pair required key value pair
+     */
+    private void put(final Pair<K, V> pair) {
+        final byte[] diffKey = diffKeyWriter.write(pair.getKey());
+        fileWriter.write(diffKey);
+        final int writenBytesInValue = valueWriter.write(fileWriter,
+                pair.getValue());
+        position = position + diffKey.length + writenBytesInValue;
+    }
 
-        /**
-         * Verify that keys are in correct order.
-         * 
-         * @param key
-         */
-        private void verifyKeyOrder(final K key) {
-                if (previousKey != null) {
-                        final int cmp = keyComparator.compare(previousKey, key);
-                        if (cmp == 0) {
-                                final String s2 = new String(keyConvertorToBytes
-                                                .toBytes(key));
-                                final String keyComapratorClassName = keyComparator
-                                                .getClass().getSimpleName();
-                                throw new IllegalArgumentException(String
-                                                .format("Attempt to insers same key as previous. Key '%s' was comapred with '%s'",
-                                                                s2,
-                                                                keyComapratorClassName));
-                        }
-                        if (cmp > 0) {
-                                final String s1 = new String(keyConvertorToBytes
-                                                .toBytes(previousKey));
-                                final String s2 = new String(keyConvertorToBytes
-                                                .toBytes(key));
-                                final String keyComapratorClassName = keyComparator
-                                                .getClass().getSimpleName();
-                                throw new IllegalArgumentException(String
-                                                .format("Attempt to insers key in invalid order. "
-                                                                + "Previous key is '%s', inserted key is '%s' and comparator is '%s'",
-                                                                s1, s2,
-                                                                keyComapratorClassName));
-                        }
-                }
-        }
+    /**
+     * Writes the given key-value pair.
+     *
+     * @param pair required key-value pair
+     */
+    public void write(final Pair<K, V> pair) {
+        Objects.requireNonNull(pair, "pair is required");
+        Objects.requireNonNull(pair.getKey(), "key is required");
+        Objects.requireNonNull(pair.getValue(), "value is required");
+        verifyKeyOrder(pair.getKey());
 
-        /**
-         * Allows to put new key value pair into index.
-         *
-         * @param pair      required key value pair
-         * @param fullWrite when it's <code>true</code> than key is written
-         *                  whole without shared part with previous key.
-         * @return position of end of record.
-         */
-        private long put(final Pair<K, V> pair, final boolean fullWrite) {
-                Objects.requireNonNull(pair, "pair is required");
-                Objects.requireNonNull(pair.getKey(), "key is required");
-                Objects.requireNonNull(pair.getValue(), "value is required");
-                verifyKeyOrder(pair.getKey());
-                previousKey = pair.getKey();
+        put(pair);
+    }
 
-                if (fullWrite) {
-                        diffKeyWriter = makeDiffKeyWriter();
-                }
+    /**
+     * Writes the given key-value pair, forcing all data to be written.
+     *
+     * @param pair required key-value pair
+     * @return position where will next data starts
+     */
+    public long writeFull(final Pair<K, V> pair) {
+        Objects.requireNonNull(pair, "pair is required");
+        Objects.requireNonNull(pair.getKey(), "key is required");
+        Objects.requireNonNull(pair.getValue(), "value is required");
+        verifyKeyOrder(pair.getKey());
 
-                final byte[] diffKey = diffKeyWriter.write(pair.getKey());
-                fileWriter.write(diffKey);
-                final int writenBytesInValue = valueWriter.write(fileWriter,
-                                pair.getValue());
+        diffKeyWriter = makeDiffKeyWriter();
 
-                long lastPosition = position;
-                position = position + diffKey.length + writenBytesInValue;
-                return lastPosition;
-        }
+        long lastPosition = position;
+        put(pair);
+        return lastPosition;
+    }
 
-        /**
-         * Writes the given key-value pair.
-         *
-         * @param pair required key-value pair
-         */
-        public void write(final Pair<K, V> pair) {
-                put(pair, false);
-        }
-
-        /**
-         * Writes the given key-value pair, forcing all data to be written.
-         *
-         * @param pair required key-value pair
-         * @return position where will next data starts
-         */
-        public long writeFull(final Pair<K, V> pair) {
-                return put(pair, true);
-        }
-
-        @Override
-        public void close() {
-                fileWriter.close();
-        }
+    @Override
+    public void close() {
+        fileWriter.close();
+    }
 
 }
--- core/src/test/java/com/coroptis/index/sorteddatafile/SortedDataFileWriterTest.java
+++ core/src/test/java/com/coroptis/index/sorteddatafile/SortedDataFileWriterTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -21,9 +20,11 @@ import com.coroptis.index.directory.FileWriter;
 @ExtendWith(MockitoExtension.class)
 public class SortedDataFileWriterTest {
 
+    private static final Pair<String, Integer> PAIR_0 = Pair.of("key0", -100);
     private static final Pair<String, Integer> PAIR_1 = Pair.of("key1", 100);
     private static final Pair<String, Integer> PAIR_2 = Pair.of("key2", 200);
-    private static final Pair<String, Integer> PAIR_3 = Pair.of("key0", 300);
+    private static final Pair<String, Integer> PAIR_3 = Pair.of("key3", 300);
+    private static final Pair<String, Integer> PAIR_4 = Pair.of("key4", 400);
 
     private final static TypeDescriptor<String> stringTd = new TypeDescriptorString();
 
@@ -36,8 +37,7 @@ public class SortedDataFileWriterTest {
     @Test
     public void test_constructor_valueWriter_is_null() {
         final Exception e = assertThrows(NullPointerException.class,
-                () -> new SortedDataFileWriter<>(null, fileWriter,
-                        stringTd));
+                () -> new SortedDataFileWriter<>(null, fileWriter, stringTd));
 
         assertEquals("valueWriter is required", e.getMessage());
     }
@@ -53,76 +53,92 @@ public class SortedDataFileWriterTest {
     @Test
     void test_constructor_keyTypeDescriptor_is_null() {
         final Exception e = assertThrows(NullPointerException.class,
-                () -> new SortedDataFileWriter<>(valueWriter, fileWriter, null));
+                () -> new SortedDataFileWriter<>(valueWriter, fileWriter,
+                        null));
 
         assertEquals("keyTypeDescriptor is required", e.getMessage());
     }
 
     @Test
     void test_write_lower_key() {
-        try (SortedDataFileWriter<String, Integer> writer = new SortedDataFileWriter<>(valueWriter, fileWriter,
-                stringTd)) {
+        try (SortedDataFileWriter<String, Integer> writer = new SortedDataFileWriter<>(
+                valueWriter, fileWriter, stringTd)) {
             writer.write(PAIR_1);
             final Exception e = assertThrows(IllegalArgumentException.class,
-                    () -> writer.write(PAIR_3));
-            assertTrue(e.getMessage().startsWith("Attempt to insers key in invalid order. "
-                    + "Previous key is 'key1', inserted key is 'key0' and comparator "
-                    + "is"));
+                    () -> writer.write(PAIR_0));
+            assertTrue(e.getMessage()
+                    .startsWith("Attempt to insers key in invalid order. "
+                            + "Previous key is 'key1', inserted key is 'key0' and comparator "
+                            + "is"));
         }
     }
 
     @Test
     void test_write_same_key() {
-        try (SortedDataFileWriter<String, Integer> writer = new SortedDataFileWriter<>(valueWriter, fileWriter,
-                stringTd)) {
+        try (SortedDataFileWriter<String, Integer> writer = new SortedDataFileWriter<>(
+                valueWriter, fileWriter, stringTd)) {
             writer.write(PAIR_1);
             final Exception e = assertThrows(IllegalArgumentException.class,
-                    () -> writer.write(PAIR_3));
-            assertTrue(e.getMessage().startsWith("Attempt to insers key in invalid order. "
-                    + "Previous key is 'key1', inserted key is 'key0' and comparator "
-                    + "is"));
+                    () -> writer.write(PAIR_0));
+            assertTrue(e.getMessage()
+                    .startsWith("Attempt to insers key in invalid order. "
+                            + "Previous key is 'key1', inserted key is 'key0' and comparator "
+                            + "is"));
         }
     }
 
     @Test
     void test_writeFull_write_same_key() {
-        try (SortedDataFileWriter<String, Integer> writer = new SortedDataFileWriter<>(valueWriter, fileWriter,
-                stringTd)) {
+        try (SortedDataFileWriter<String, Integer> writer = new SortedDataFileWriter<>(
+                valueWriter, fileWriter, stringTd)) {
             writer.writeFull(PAIR_1);
             final Exception e = assertThrows(IllegalArgumentException.class,
-                    () -> writer.write(PAIR_3));
-            assertTrue(e.getMessage().startsWith("Attempt to insers key in invalid order. "
-                    + "Previous key is 'key1', inserted key is 'key0' and comparator "
-                    + "is"));
+                    () -> writer.write(PAIR_0));
+            assertTrue(e.getMessage()
+                    .startsWith("Attempt to insers key in invalid order. "
+                            + "Previous key is 'key1', inserted key is 'key0' and comparator "
+                            + "is"));
         }
     }
 
     @Test
     public void test_write() {
-        try (SortedDataFileWriter<String, Integer> writer = new SortedDataFileWriter<>(valueWriter, fileWriter,
-                stringTd)) {
+        try (SortedDataFileWriter<String, Integer> writer = new SortedDataFileWriter<>(
+                valueWriter, fileWriter, stringTd)) {
             writer.write(PAIR_1);
             verify(valueWriter).write(fileWriter, 100);
+            writer.write(PAIR_2);
+            verify(valueWriter).write(fileWriter, 200);
         }
     }
-
-    //TODO add tests write & writeFull
 
     @Test
-    @Disabled
-    public void test_writeFull() {
-        try (SortedDataFileWriter<String, Integer> writer = new SortedDataFileWriter<>(valueWriter, fileWriter,
-                stringTd)) {
-            when(valueWriter.write(fileWriter, 100)).thenReturn(23);
+    public void test_writeFull_all_writes_are_full() {
+        try (SortedDataFileWriter<String, Integer> writer = new SortedDataFileWriter<>(
+                valueWriter, fileWriter, stringTd)) {
             long ret = writer.writeFull(PAIR_1);
-
             assertEquals(0, ret);
 
-            when(valueWriter.write(fileWriter, 200)).thenReturn(77);
-            ret = writer.writeFull(PAIR_2);
+            writer.write(PAIR_2);
+            ret = writer.writeFull(PAIR_3);
+            assertEquals(9, ret);
 
-            assertEquals(60, ret);
+            ret = writer.writeFull(PAIR_4);
+            assertEquals(15, ret);
         }
     }
-    
+
+    @Test
+    public void test_writeFull_mixed() {
+        try (SortedDataFileWriter<String, Integer> writer = new SortedDataFileWriter<>(
+                valueWriter, fileWriter, stringTd)) {
+            writer.write(PAIR_1);
+            writer.write(PAIR_2);
+            writer.write(PAIR_3);
+
+            long ret = writer.writeFull(PAIR_4);
+            assertEquals(12, ret);
+        }
+    }
+
 }
