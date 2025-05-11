5cc3d5e62b72d468a1d1322c82b21752b154a5de
Jan Jirout <jirout@gmail.com>
2025-05-11 08:06:29 +0200

    logger wrapper name is not in final log messages #61

--- core/src/main/java/com/coroptis/index/ContextAwareLogger.java
+++ core/src/main/java/com/coroptis/index/ContextAwareLogger.java
@@ -2,10 +2,12 @@ package com.coroptis.index;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.spi.LocationAwareLogger;
 
 public class ContextAwareLogger {
-    private final Logger logger;
+    private final LocationAwareLogger logger;
     private final LoggingContext context;
+    private final String fqcn = ContextAwareLogger.class.getName();
 
     public ContextAwareLogger(Class<?> clazz, LoggingContext context) {
         if (clazz == null) {
@@ -14,46 +16,55 @@ public class ContextAwareLogger {
         if (context == null) {
             throw new IllegalArgumentException("LoggingContext cannot be null");
         }
-        this.logger = LoggerFactory.getLogger(clazz);
+        final Logger rawLogger = LoggerFactory.getLogger(clazz);
+        if (!(rawLogger instanceof LocationAwareLogger)) {
+            throw new IllegalStateException(
+                    "Logger must be LocationAwareLogger (use Logback or Log4j)");
+        }
+        this.logger = (LocationAwareLogger) rawLogger;
         this.context = context;
-    }
-
-    private String format(String message) {
-        return context.formatPrefix() + message;
     }
 
     public void trace(String message, Object... args) {
         if (logger.isTraceEnabled()) {
-            logger.trace(format(message), args);
+            log(LocationAwareLogger.TRACE_INT, message, args);
         }
     }
 
     public void debug(String message, Object... args) {
         if (logger.isDebugEnabled()) {
-            logger.debug(format(message), args);
+            log(LocationAwareLogger.DEBUG_INT, message, args);
         }
     }
 
     public void info(String message, Object... args) {
         if (logger.isInfoEnabled()) {
-            logger.info(format(message), args);
+            log(LocationAwareLogger.INFO_INT, message, args);
         }
     }
 
     public void warn(String message, Object... args) {
         if (logger.isWarnEnabled()) {
-            logger.warn(format(message), args);
+            log(LocationAwareLogger.WARN_INT, message, args);
         }
     }
 
     public void error(String message, Object... args) {
         if (logger.isErrorEnabled()) {
-            logger.error(format(message), args);
+            log(LocationAwareLogger.ERROR_INT, message, args);
         }
     }
 
     // Optionally expose the underlying SLF4J logger if needed
     public Logger getUnderlyingLogger() {
         return logger;
+    }
+
+    private final void log(int level, String message, Object... args) {
+        logger.log(null, fqcn, level, format(message), args, null);
+    }
+
+    private String format(String message) {
+        return context.formatPrefix() + message;
     }
 }
--- core/src/test/java/com/coroptis/index/ContextAwareLoggerTest.java
+++ core/src/test/java/com/coroptis/index/ContextAwareLoggerTest.java
@@ -2,11 +2,15 @@ package com.coroptis.index;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.coroptis.index.segment.Segment;
 
 @ExtendWith(MockitoExtension.class)
 public class ContextAwareLoggerTest {
@@ -29,5 +33,30 @@ public class ContextAwareLoggerTest {
                         null));
 
         assertEquals("LoggingContext cannot be null", e.getMessage());
+    }
+
+    @Test
+    void test_log_debug() {
+        when(loggingContext.formatPrefix()).thenReturn("");
+        final ContextAwareLogger logger = new ContextAwareLogger(Segment.class,
+                loggingContext);
+
+        logger.debug("It's '{}'", 4);
+        logger.debug("It's '{}'", 42);
+        assertTrue(true);
+    }
+
+    @Test
+    void test_log_allLevels() {
+        when(loggingContext.formatPrefix()).thenReturn("");
+        final ContextAwareLogger logger = new ContextAwareLogger(Segment.class,
+                loggingContext);
+
+        logger.trace("It's '{}'", "trace");
+        logger.debug("It's '{}'", "debug");
+        logger.info("It's '{}'", "info");
+        logger.warn("It's '{}'", "warn");
+        logger.error("It's '{}'", "error");
+        assertTrue(true);
     }
 }
